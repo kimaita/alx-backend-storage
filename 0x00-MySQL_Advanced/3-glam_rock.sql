59364777ed3ca7cb4624141dd0633906f48b5a34
kimaita <johnnieqaym@gmail.com>
2024-08-22 23:00:56 +0300

    changed year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 --Lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name,
-    (COALESCE(split, 2022) - formed) AS lifespan
+    (COALESCE(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE LOWER(style) LIKE '%glam rock%'
 ORDER BY lifespan DESC;
