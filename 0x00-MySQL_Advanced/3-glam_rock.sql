3c4fb4e2eb2b21090ab3d69ce5b360adbbf1862e
kimaita <johnnieqaym@gmail.com>
2024-08-23 13:42:13 +0300

    fixed comment

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---Lists all bands with Glam rock as their main style, ranked by their longevity
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style)
