2170b89772aa7b6802403b7cf89a1cb2eb65fff4
kimaita <johnnieqaym@gmail.com>
2024-08-27 00:29:32 +0300

    account for empty cursors
    Return an empty list in case there are no documents in the collection.

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Contains a function for listing MongoDB documents"""
 
+
 def list_all(mongo_collection):
-    """"""
-    return mongo_collection.find({})
+    """Returns all documents in the given MongoDB collection"""
+    cursor = mongo_collection.find({})
+    if cursor.collection.estimated_document_count():
+        return cursor
+    return []
